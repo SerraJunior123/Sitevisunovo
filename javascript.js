f87a76a292862088e0e23b83cf2d88fceb3c9bd8
SerraJunior123 <98670408+SerraJunior123@users.noreply.github.com>
2022-09-11 07:17:37 -0300

    toglle
    menu togle

--- javascript.js
+++ javascript.js
@@ -1,1 +1,17 @@
 
+
+
+function Enviar( a,b){
+
+document.getElementById("btn1").style.display="none";
+document.getElementById("form").style.display="none";
+document.getElementById("img").classList.add("imagem");
+document.getElementById("voltar").style.display="block";
+event.preventDefault();
+
+    
+}
+
+
+
+
